366927367b2fd2c6dce5192590a3f0bdb506c219
pandeypriti015 <pandeypriti015@gmail.com>
2019-12-02 14:54:50 +0530

    to_do_list

--- to_do/src/App.js
+++ to_do/src/App.js
@@ -1,43 +1,81 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class List extends Component{
-constructor(props) {
-  super(props);
-  this.state=
-  {
-  }
-}
-
-render() {
-  return (
-    <div className='body'>
-      <div className='body-header'>
-        <textarea rows="10" cols="50"/>
-        <button className="button">
-        <div className="text"> Add </button>
-        <
-    </div>
-      </div>
-  )
-  
-}
-}
-
-class App extends Component {
- 
-  render(){
-    return (
-      <div className='body'>
-      <div className='body-header'>
-      <div className="text">
-      </div>
-        <List />
-      </div>
-      </div>
-    );     
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      pageCount:240
     }
+    this.wordCount = this.wordCount.bind(this);
+    this.addText = this.addText.bind(this);
   }
-  
+  addText() {
+    let text = this.state.text;
+    this.props.setItems(text);
+    this.setState({
+      text:'',
+      pageCount:240
+    })
+  }
+
+  wordCount(event){
+    let characterCount =240- event.target.value.length;
+    this.setState({
+      pageCount: characterCount,
+      text: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='App-header'>
+          <div className='header-content'>
+            <div className='textarea-container'>
+              <textarea className='textarea' maxLength='240' onChange={this.wordCount} 
+              value={this.state.text} placeholder='WElCOME'></textarea>
+            </div>
+            <div >
+              <button className='button' onClick={this.addText}>Add</button>
+            </div>
+          </div>
+          <span className="md-color-grey-300">Current word {this.state.pageCount}</span>
+        </div>
+      </div>
+    );
+  }
+}
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text:'',
+      items:[]
+
+    };
+    this.setItems = this.setItems.bind(this);
+  }
+  setItems(text) {
+    this.setState({
+      text: text,
+      items:[...this.state.items,text]
+    });
+  }
+  render(){
+    return(
+      <div>
+         <Todo setItems={this.setItems} />
+         {this.state.items.map((item, index) => (
+           <div key='index'>
+             <input type="checkbox"  className="check"/>
+              <h1>{item}</h1>
+            </div>
+         ))
+  }
+  </div>
+    )
+  }
+}
 export default App;
